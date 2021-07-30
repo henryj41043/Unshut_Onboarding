a94213cbe5f435fc76d8b269959344bbd6f99ed0
NeryCaballero <nerycaballero24@gmail.com>
2021-07-30 10:52:27 +0200

    Created Questions API.

---
+++ src/api/QuizAPI.js
@@ -0,0 +1,185 @@
+const questionBank = [
+    {
+        questionId: 1,
+        questionText: "What is the right definition of Infrasound?",
+        questionAnswers: [
+            {   
+                answerId: 1,
+                answerText: "Low-frequency sound (under 20hz).",
+                isCorrect: false
+            },
+            {   
+                answerId: 2,
+                answerText: "The frequency range used in monitoring earthquakes and volcanoes.",
+                isCorrect: false
+            },
+            {   
+                answerId: 3,
+                answerText: "A technique for studying ballistocardiography and seismocardiology (the mechanics of the heart).",
+                isCorrect: false
+            },
+            {   
+                answerId: 4,
+                answerText: "A sonic method for detecting artillery used in World War I.",
+                isCorrect: false
+            },
+            {   
+                answerId: 5,
+                answerText: "The cause of frenzied, violent death for 9 Russian hikers in the Dyatlov Pass Incident.",
+                isCorrect: false
+            },
+            {   
+                answerId: 6,
+                answerText: "The resonant frequency of the human eyeball, which can produce hallucinations when stimulated.",
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        questionId: 2,
+        questionText: "What is the right definition of Gloaming?",
+        questionAnswers: [
+            {   
+                answerId: 1,
+                answerText: "A 2003 song by Radiohead.",
+                isCorrect: false
+            },
+            {   
+                answerId: 2,
+                answerText: "The middle ground between light and shadow.",
+                isCorrect: false
+            },
+            {   
+                answerId: 3,
+                answerText: "A mold found in the crevices of certain southwest acequias.",
+                isCorrect: false
+            },
+            {   
+                answerId: 4,
+                answerText: "FREE TEXT",
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        questionId: 3,
+        questionText: "What is the right (best) definition of Revolution?",
+        questionAnswers: [
+            {   
+                answerId: 1,
+                answerText: "An overthrow and replacement of an established system or social structure, often accompanied by violence.",
+                isCorrect: false
+            },
+            {   
+                answerId: 2,
+                answerText: "A single rotation on one axis by 360 degrees.",
+                isCorrect: false
+            },
+            {   
+                answerId: 3,
+                answerText: "The period of time it takes an object to fully circle around another object.",
+                isCorrect: false
+            },
+            {   
+                answerId: 4,
+                answerText: "A round or cycle of events in time or in a recurring period of time.",
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        questionId: 4,
+        questionText: "What is the right definition of Syzygy?",
+        questionAnswers: [
+            {   
+                answerId: 1,
+                answerText: "A linear alignment of 3 celestial bodies.",
+                isCorrect: false
+            },
+            {   
+                answerId: 2,
+                answerText: "Any pair of related things, either alike or opposite.",
+                isCorrect: false
+            },
+            {   
+                answerId: 3,
+                answerText: "The CIA’s official terminology for “double-walker” or “dopplegänger”.",
+                isCorrect: false
+            },
+            {   
+                answerId: 4,
+                answerText: "FREETEXT.",
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        questionId: 5,
+        questionText: "What is 6 x 8?",
+        questionAnswers: [
+            {   
+                answerId: 1,
+                answerText: "45",
+                isCorrect: false
+            },
+            {   
+                answerId: 2,
+                answerText: "68",
+                isCorrect: false
+            },
+            {   
+                answerId: 3,
+                answerText: "48",
+                isCorrect: false
+            },
+            {   
+                answerId: 4,
+                answerText: "51",
+                isCorrect: false
+            },
+            {   
+                answerId: 5,
+                answerText: "FREE TEXT.",
+                isCorrect: false
+            },
+            {   
+                answerId: 6,
+                answerText: "",
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        questionId: 6,
+        questionText: "What is measured in Hertz?",
+        questionAnswers: [
+            {   
+                answerId: 1,
+                answerText: "Heat",
+                isCorrect: false
+            },
+            {   
+                answerId: 2,
+                answerText: "Glare",
+                isCorrect: false
+            },
+            {   
+                answerId: 3,
+                answerText: "Electricity",
+                isCorrect: false
+            },
+            {   
+                answerId: 4,
+                answerText: "Frequency",
+                isCorrect: false
+            },
+            {   
+                answerId: 5,
+                answerText: "FREETEXT.",
+                isCorrect: false
+            }
+        ]
+    }
+]
+
+export default questionBank;
