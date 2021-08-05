51716a9692b6385f167f376d50d4aa738efeaff9
NeryCaballero <nerycaballero24@gmail.com>
2021-08-05 17:33:00 +0200

    Added Context. Added resetTimer functionality onClick.

--- src/components/QuizQuestion.js
+++ src/components/QuizQuestion.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from './App'
 
 export default function QuizQuestion( props ) {
 
-    const { question, answers, handleAnswerButtonClick, handleSelectedAnswers } = props
-    
+    const { question, answers } = props
+    const { handleAnswerButtonClick, handleSelectedAnswers, resetTimer } = useContext(AppContext)
     return (
         <div className="quiz-question-container">
             <p className="quiz-question">{question}</p>
@@ -13,6 +14,7 @@ export default function QuizQuestion( props ) {
                     onClick={()=>{
                         handleSelectedAnswers(answer.answerText);
                         handleAnswerButtonClick();
+                        resetTimer();
                     }}
                     key={answer.answerId}>
                     {answer.answerText}
