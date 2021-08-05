1acbfc2e1cb912559344ee83a158a0d4f8f28bb3
NeryCaballero <nerycaballero24@gmail.com>
2021-08-05 17:31:27 +0200

    Moved useStates and useEffects to App.js. Added useContext. Solved missing zero on seconds count.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from './App'
 
 export default function Counter() {
-    const [seconds, setSeconds] = useState(5);
-    const [minutes, setMinutes] = useState(1);
+    const { seconds, minutes } = useContext(AppContext)
 
-    useEffect(() => {
-      if (seconds > 0) {
-        setTimeout(() => setSeconds(seconds - 1), 1000);
-      }
-      else if(seconds === 0) {
-        setMinutes(0)
-        setSeconds(10)
-      }
-    })
-
-    if (minutes === 0 && seconds === 0) {
-        setSeconds('time over')
-        // next question
+    if (seconds >= 10) {
+      return (
+        <>
+          
+          <p className="counter">00:0{minutes}:{seconds}</p>
+          {console.log(seconds)}
+        </>
+      )
+    } else {
+      return (
+        <>
+          
+          <p className="counter">00:0{minutes}:0{seconds}</p>
+          {console.log(seconds)}
+        </>
+      )
     }
     
-
-    return (
-        <>
-            <p className="counter">00:0{minutes}:{seconds}</p>
-        </>
-    )
 }
